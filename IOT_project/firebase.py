9715d3a8f0aac1fe7298590f6edd59b31d36b846
Lian Jiade <jdlian505@gmail.com>
2021-11-01 20:54:06 +0800

    Update firebase.py

--- IOT_project/firebase.py
+++ IOT_project/firebase.py
@@ -9,17 +9,50 @@ def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Successfully connected to broker")
         client.subscribe("sensor/data")
+        client.subscribe("sensor_peak/data")
+
     else:
         print("Connection failed with code %d." %rc)
 
 
 def on_message(client, userdata, msg):
     recv_dict = json.loads(msg.payload)
-    value = recv_dict['result']
-    ref = db.reference('message')
-    current_value = ref.get()
-    if current_value != value:
-        print("changed")
+
+    print(recv_dict)
+
+    if recv_dict['type'] == 'activity':
+        value = recv_dict['result']
+        ref = db.reference('message')
+        current_value = ref.get()
+        if current_value != value:
+            print("changed")
+            ref.set(value)
+
+    if recv_dict['type'] == 'peak_pushup':
+        value = recv_dict['peak_count']
+        ref = db.reference('pushup_count')
+        current_value = ref.get()
+        print(current_value)
+        value = int(current_value) + int(value)
+        print(value)
+        ref.set(value)
+
+    if recv_dict['type'] == 'peak_situp':
+        value = recv_dict['peak_count']
+        ref = db.reference('situp_count')
+        current_value = ref.get()
+        print(current_value)
+        value = int(current_value) + int(value)
+        print(value)
+        ref.set(value)
+
+    if recv_dict['type'] == 'peak_jump':
+        value = recv_dict['peak_count']
+        ref = db.reference('jump_count')
+        print(current_value)
+        current_value = ref.get()
+        value = int(current_value) + int(value)
+        print(value)
         ref.set(value)
 
 def setup(hostname):
@@ -44,5 +77,6 @@ if __name__ == "__main__":
         'databaseURL': 'https://nusiot-4b3c2-default-rtdb.asia-southeast1.firebasedatabase.app/'
     })
     main()
+
 
 
